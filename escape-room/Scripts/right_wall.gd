4f451b113d2c8d9ec93b976a4a26df28a901fc17
yangying-liu <liuyangying2@gmail.com>
2025-03-02 17:22:49 -0800

    change scene files

--- escape-room/Scripts/right_wall.gd
+++ escape-room/Scripts/right_wall.gd
@@ -3,4 +3,4 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-		get_tree().change_scene_to_file("res://main.tscn")
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
--- escape-room/Scripts/wall_1.gd
+++ escape-room/Scripts/wall_1.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
