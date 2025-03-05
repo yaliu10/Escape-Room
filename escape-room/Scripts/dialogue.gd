6dc56413819d56eb374de1f899419cd358c61c66
yaliu10 <yaliu10@s.sfusd.edu>
2025-03-05 13:48:45 -0800

    Dialogue Coding

---
+++ escape-room/Scripts/dialogue.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+var dialogue_items: Array[String] = [
+	"Hey",
+	"LALA.",
+	"Haha",
+	"Hehe",
+	"Bye bye~!",
+]
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		hide()
+	else:
+		show_text()
