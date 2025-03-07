1619388fa8055cc427e48a5e99cbd057f80ca360
yaliu10 <yaliu10@s.sfusd.edu>
2025-03-07 15:48:42 -0800

    Editing the dialogue property

--- escape-room/Scripts/dialogue.gd
+++ escape-room/Scripts/dialogue.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
 
-var dialogue_items: Array[String] = [
+@export var dialogue_items: Array[String] = [
 	"Hey",
 	"LALA.",
 	"Haha",
--- escape-room/Scripts/main.gd
+++ escape-room/Scripts/main.gd
@@ -11,3 +11,9 @@ func _on_right_button_pressed() -> void:
 func _on_left_button_pressed() -> void:
 	var game = preload ("res://Scenes/left_wall.tscn").instantiate()
 	get_tree().get_root().add_child(game)
+
+
+
+func _on_text_object_pressed() -> void:
+	var game = preload ("res://Scenes/dialogue.tscn").instantiate()
+	get_tree().get_root().add_child(game)
