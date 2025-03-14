338d3248e0f77d2ec586010699f0a897f81f9ecc
yaliu10 <yaliu10@s.sfusd.edu>
2025-03-14 14:57:06 -0700

    Start Menu Completed

---
+++ escape-room/Scripts/start_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_credit_button_pressed() -> void:
+	pass # Replace with function body.
