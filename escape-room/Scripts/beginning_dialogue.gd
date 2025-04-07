a7af6d883620a250b8cedf1ef06b57195bd80b7c
yaliu10 <yaliu10@s.sfusd.edu>
2025-04-07 15:34:28 -0700

    Complete making a beginning dialogue

---
+++ escape-room/Scripts/beginning_dialogue.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+@export var dialogue_items: Array[String] = [
+	"Hey",
+	"LALA.",
+	"Haha",
+	"Hehe",
+	"Bye bye~!",
+]
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	else:
+		show_text()
--- escape-room/Scripts/start_menu.gd
+++ escape-room/Scripts/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/beginning_dialogue.tscn")
 
 
 func _on_quit_button_pressed() -> void:
