211c90c1caf99fc582bb377643c95b34b9850b4f
yaliu10 <yaliu10@s.sfusd.edu>
2025-04-07 15:23:39 -0700

    Succeed pause icon adding

--- escape-room/Scripts/main.gd
+++ escape-room/Scripts/main.gd
@@ -4,9 +4,11 @@ func _on_up_button_pressed() -> void:
 	var game = preload ("res://Scenes/wall_1.tscn").instantiate()
 	get_tree().get_root().add_child(game)
 
+
 func _on_right_button_pressed() -> void:
 	var game = preload ("res://Scenes/right_wall.tscn").instantiate()
 	get_tree().get_root().add_child(game)
+
 
 func _on_left_button_pressed() -> void:
 	var game = preload ("res://Scenes/left_wall.tscn").instantiate()
@@ -17,14 +19,3 @@ func _on_left_button_pressed() -> void:
 func _on_text_object_pressed() -> void:
 	var game = preload ("res://Scenes/dialogue.tscn").instantiate()
 	get_tree().get_root().add_child(game)
-
-
-func _on_pause_icon_pressed() -> void:
-	print("Pause button clicked!")  # Check if this appears in the Output
-	var pause_screen = preload("res://Scenes/pause_screen.tscn").instantiate()
-	if pause_screen:
-		print("Pause scene loaded successfully!")  # Check if this appears
-		get_tree().get_root().add_child(pause_screen)
-		get_tree().paused = true  # Pause the game
-	else:
-		print("Error: Failed to load pause screen!")  # Check if this appears
--- escape-room/Scripts/pause_screen.gd
+++ escape-room/Scripts/pause_screen.gd
@@ -1,17 +1,25 @@
 extends Control
 
+
 var _is_paused: bool = false:
 	set(value):
 		_is_paused = value
 		get_tree().paused = _is_paused
 		visible = _is_paused
 
+
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("pause"):
 		_is_paused = !_is_paused
 
+
 func _on_resume_button_pressed() -> void:
 	_is_paused = false
 
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_pause_icon_pressed() -> void:
+	_is_paused = !_is_paused
