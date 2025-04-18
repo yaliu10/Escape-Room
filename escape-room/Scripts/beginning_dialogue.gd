2b6a857b02887d101c2fa1b609c55e7ccb1af29c
yaliu10 <yaliu10@s.sfusd.edu>
2025-04-18 15:04:22 -0700

    Animating the text

--- escape-room/Scripts/beginning_dialogue.gd
+++ escape-room/Scripts/beginning_dialogue.gd
@@ -20,6 +20,12 @@ func _ready() -> void:
 func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item
+	rich_text_label.visible_ratio = 0.0
+
+	var tween := create_tween()
+	var text_appearing_duration := 0.3
+	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+	
 
 func advance() -> void:
 	current_item_index += 1
