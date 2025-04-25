9c43318c15f2f98d3c7f985908da9d6b716cb964
Yujing14 <yuyu1@s.sfusd.edu>
2025-04-25 13:52:31 -0700

    Inputting Vase Image

--- escape-room/texture_rect.gd
+++ escape-room/texture_rect.gd
@@ -1,26 +1,10 @@
 extends TextureRect
 
 func _get_drag_data(at_position):
-	
-	var preview_texture = TextureRect.new()
-	
-	preview_texture.texture = texture
-	preview_texture.expand_mode = 1
-	preview_texture.size = Vector2(30,30)
-	
-	var preview = Control.new()
-	preview.add_child(preview_texture)
-	
-	set_drag_preview(preview)
-	texture = null
-	
-	
-	return preview_texture.texture
-
+	pass
 
 func _can_drop_data(_pos, data):
-	return data is Texture2D
-
+	pass
 
 func _drop_data(_pos, data):
-	texture = data
+	pass
