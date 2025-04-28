690eb6d14b830128b24fd70438f688b210bf2f75
joszeto1 <joszeto1@s.sfusd.edu>
2025-04-28 01:27:17 -0700

    inventory done!!!!!!!!!!!!!!!!!!!!!!

--- escape-room/Scripts/item.gd
+++ escape-room/Scripts/item.gd
@@ -1,12 +1,43 @@
 extends Sprite2D
 
+var grab_cursor = preload("res://art/IMG_2834.PNG")
+var hand_cursor = preload("res://art/IMG_2835.PNG")
+
 var dragging = false
-var grab_cursor = preload("res://art/ph-slot.PNG")#placeholder
 var picked_up = false
 var drop_location_id = 0
 var drop_location
+var dropped = false
+var timer_off = true
+
 @export var item_id = 1
 
 func _physics_process(delta: float) -> void:
 	if dragging:
 		global_position = lerp(global_position, get_global_mouse_position(), 30 * delta)
+		dropped = false
+		Input.set_custom_mouse_cursor(grab_cursor)
+	else:
+		if picked_up:
+			if !dropped:
+				drop_location = get_parent().set_drop_location(self)
+				if timer_off:
+					$Timer.start()
+					timer_off = false
+			global_position = lerp(global_position, drop_location, 20 * delta)
+
+func _on_test_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if !picked_up and !dragging:
+			global_position = get_parent().choose_slot(self)
+			z_index = 10
+			picked_up = true
+		else:
+			if event.pressed:
+				dragging = true
+			else:
+				dragging = false
+
+func _on_timer_timeout() -> void:
+	dropped = true
+	timer_off = true
--- escape-room/room.gd
+++ escape-room/room.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var cursor = preload("res://art/IMG_2835.PNG")
+var grab = preload("res://art/IMG_2834.PNG")
+
+var slot_1_filled = false
+var slot_2_filled = false
+var slot_3_filled = false
+var slot_4_filled = false
+var slot_5_filled = false
 
 
 #right now the same variable is being used to check every wall, 
@@ -39,6 +46,63 @@ func change_cursor_in_painting():
 func change_cursor_back_in_painting():
 	Input.set_custom_mouse_cursor(null)
 	
+	
+func fill_slot(slot):
+	match slot:
+		1: slot_1_filled = true
+		2: slot_2_filled = true
+		3: slot_3_filled = true
+		4: slot_4_filled = true
+		5: slot_5_filled = true
+
+func empty_slot(slot):
+	match slot:
+		1: slot_1_filled = false
+		2: slot_2_filled = false
+		3: slot_3_filled = false
+		4: slot_4_filled = false
+		5: slot_5_filled = false
+		
+func choose_slot(item):
+	var chosen_slot = 1
+	if !slot_1_filled:
+		chosen_slot = $Slot1.global_position
+		slot_1_filled = true
+		item.drop_location_id = 1
+		
+	elif !slot_2_filled:
+		chosen_slot = $Slot2.global_position
+		slot_2_filled = true
+		item.drop_location_id = 2
+	elif !slot_3_filled:
+		chosen_slot = $Slot3.global_position
+		slot_3_filled = true
+		item.drop_location_id = 3
+		
+	elif !slot_4_filled:
+		chosen_slot = $Slot4.global_position
+		slot_4_filled = true
+		item.drop_location_id = 4
+		
+	elif !slot_5_filled:
+		chosen_slot = $Slot5.global_position
+		slot_5_filled = true
+		item.drop_location_id = 5
+		
+	return chosen_slot
+
+func set_drop_location(item):
+	var drop_location
+	match item.drop_location_id:
+		0: drop_location = item.global_position
+		1: drop_location = $Slot1.global_position
+		2: drop_location = $Slot2.global_position
+		3: drop_location = $Slot3.global_position
+		4: drop_location = $Slot4.global_position
+		5: drop_location = $Slot5.global_position
+	return drop_location
+
+
 #left wall selection
 func _on_left_return_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
@@ -109,3 +173,48 @@ func _on_front_painting_area_input_event(viewport: Node, event: InputEvent, shap
 		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 			$"In Front Painting".visible = true
 			painting_selected = true
+
+
+func _on_slot_1_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item") and !slot_1_filled:
+		empty_slot(area.get_parent().drop_location_id)
+		area.get_parent().drop_location_id = 1
+		fill_slot(area.get_parent().drop_location_id)
+
+func _on_slot_2_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item") and !slot_2_filled:
+		empty_slot(area.get_parent().drop_location_id)
+		area.get_parent().drop_location_id = 2
+		fill_slot(area.get_parent().drop_location_id)
+
+func _on_slot_3_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item") and !slot_3_filled:
+		empty_slot(area.get_parent().drop_location_id)
+		area.get_parent().drop_location_id = 3
+		fill_slot(area.get_parent().drop_location_id)
+
+func _on_slot_4_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item") and !slot_4_filled:
+		empty_slot(area.get_parent().drop_location_id)
+		area.get_parent().drop_location_id = 4
+		fill_slot(area.get_parent().drop_location_id)
+		
+func _on_slot_5_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item") and !slot_5_filled:
+		empty_slot(area.get_parent().drop_location_id)
+		area.get_parent().drop_location_id = 5
+		fill_slot(area.get_parent().drop_location_id)
+
+
+#need to hide visibility of objects when switching rooms
+func _on_test_interact_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item"):
+		if area.get_parent().item_id == 1:
+			$TestInteract.visible = false
+			$TestInteract2.visible = true
+			#estitem_used = true
+			
+			empty_slot(area.get_parent().drop_location_id)
+			area.get_parent().queue_free()
+			Input.set_custom_mouse_cursor(cursor)
+	
