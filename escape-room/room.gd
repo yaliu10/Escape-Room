d844e253b6d3d0ea69ec80442a371b061913af0f
joszeto1 <joszeto1@s.sfusd.edu>
2025-04-30 13:51:03 -0700

    more interactions

--- escape-room/room.gd
+++ escape-room/room.gd
@@ -207,14 +207,27 @@ func _on_slot_5_area_area_entered(area: Area2D) -> void:
 
 
 #need to hide visibility of objects when switching rooms
+#error comes from item group
 func _on_test_interact_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("item"):
 		if area.get_parent().item_id == 1:
-			$TestInteract.visible = false
-			$TestInteract2.visible = true
-			#estitem_used = true
+			$Open.visible = false
+			$Closed.visible = true
+			$Key.visible = true
+			#testitem_used = true
 			
 			empty_slot(area.get_parent().drop_location_id)
 			area.get_parent().queue_free()
 			Input.set_custom_mouse_cursor(cursor)
-	
+
+func _on_slot_area_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item"):
+		if area.get_parent().item_id == 2:
+			$Open.visible = false
+			$Closed.visible = true
+			$Key.visible = true
+			#testitem_used = true
+			
+			empty_slot(area.get_parent().drop_location_id)
+			area.get_parent().queue_free()
+			Input.set_custom_mouse_cursor(cursor)
