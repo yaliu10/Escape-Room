444e0341e561ac2f959495596f63527f804c2515
yaliu10 <yaliu10@s.sfusd.edu>
2025-04-30 13:40:42 -0700

    scene in progress

---
+++ escape-room/Scripts/ending.gd
@@ -0,0 +1,22 @@
+extends Control
+
+var detail_selected = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		$Placeholder.visible = false
+		detail_selected = false
+
+
+func _on_right_button_pressed() -> void:
+	$Placeholder.visible = true
