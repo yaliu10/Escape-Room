4c2d8e3409a7484acc995710da40e9301a8187a9
yaliu10 <yaliu10@s.sfusd.edu>
2025-05-02 15:48:56 -0700

    Scene completed (need some sizing for 3rd)

--- escape-room/Scripts/ending.gd
+++ escape-room/Scripts/ending.gd
@@ -2,21 +2,32 @@ extends Control
 
 var detail_selected = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		$Placeholder.visible = false
-		detail_selected = false
+	if !detail_selected:
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+			$TextureRect.visible = false
+			detail_selected = false
 
-
-func _on_right_button_pressed() -> void:
+func _on_forward_button_pressed() -> void:
 	$Placeholder.visible = true
+
+
+func _on_nd_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if !detail_selected:
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+			$Placeholder.visible = false
+			detail_selected = false
+
+func _on_nd_button_pressed() -> void:
+	$Placeholder2.visible = true
+
+
+func _on_rd_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if !detail_selected:
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+			$Placeholder2.visible = false
+			detail_selected = false
+
+
+func _on_rd_button_pressed() -> void:
+	pass
