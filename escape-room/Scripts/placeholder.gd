2b782cb0d656bf2e71c29273c048311103359224
yaliu10 <yaliu10@s.sfusd.edu>
2025-05-05 15:52:39 -0700

    Work in progress

---
+++ escape-room/Scripts/placeholder.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		Label.text += 1
+		$Sprite2D.queue_free()
