061a9d17f074f4debb379971cfa2756f79431fdc
yaliu10 <yaliu10@s.sfusd.edu>
2025-05-09 15:25:41 -0700

    Redo Completed

--- escape-room/Scripts/ending.gd
+++ escape-room/Scripts/ending.gd
@@ -2,28 +2,21 @@ extends Node2D
 
 var detail_selected = false
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if !detail_selected:
-		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-			$Sprite2D.visible = false
-			detail_selected = false
-
-func _on_forward_button_pressed() -> void:
-	$Sprite2D2.visible = true
-
+func _on_st_button_pressed() -> void:
+	$"2ndScene".visible = true
 
 func _on_nd_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if !detail_selected:
 		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-			$Sprite2D.visible = false
+			$"2ndScene".visible = false
 			detail_selected = false
 
 func _on_nd_button_pressed() -> void:
-	$Sprite2D3.visible = true
+	$"3rdScene".visible = true
 
 
 func _on_rd_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if !detail_selected:
 		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-			$Sprite2D3.visible = false
+			$"3rdScene".visible = false
 			detail_selected = false
