0b513440bb734b7d8d8a7e168949d04066d86e81
yaliu10 <yaliu10@s.sfusd.edu>
2025-05-20 15:50:10 -0700

    Credit scene in progress

--- escape-room/Scripts/start_menu.gd
+++ escape-room/Scripts/start_menu.gd
@@ -10,4 +10,4 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_credit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/memory_pieces.tscn")
